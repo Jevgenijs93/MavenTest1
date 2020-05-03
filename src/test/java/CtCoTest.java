4f98259b0f41069a3a9cf9ad7f8c0f36b3f42f2e
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2020-05-03 18:38:46 +0300

    Visit C.T.Co page

---
+++ src/test/java/CtCoTest.java
@@ -0,0 +1,28 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CtCoTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ctco.lv/en");
+    }
+
+    @Test
+    public void userLogin() {
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+    }
+
+}
