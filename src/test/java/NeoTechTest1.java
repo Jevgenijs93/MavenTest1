dc52b13e93873648161e8cb1de29ded4c37d6628
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2020-06-07 15:59:05 +0300

    NeotechTest1

--- src/test/java/NeoTechTest1.java
+++ src/test/java/NeoTechTest1.java
@@ -25,14 +25,18 @@ public class NeoTechTest1 {
       //  driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS) ;
         WebElement VisiPiedavajumi = driver.findElement(By.xpath("//li[5]/a/span"));
         VisiPiedavajumi.click();
+        driver.manage().timeouts().implicitlyWait(1,TimeUnit.SECONDS);
         WebElement HuaweiMobiles = driver.findElement(By.xpath("//a[contains(@href, 'https://www.rdveikals.lv/promoaction/lv/444/')]"));
         HuaweiMobiles.click();
         driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
         WebElement HuaweiMobile1 = driver.findElement(By.xpath("//div[4]/div/div/ul/li/a"));
         HuaweiMobile1.click();
-        driver.get("https://www.rdveikals.lv/promoaction/lv/444/page/1/");
-        WebElement HuaweiMobile2 = driver.findElement(By.xpath("//div[4]/div/div/ul/li[3]/a"));
-        HuaweiMobile2.click();
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+        WebElement AddToCart1 = driver.findElement(By.xpath("//div[2]/div[4]/a"));
+        AddToCart1.click();
+      //  driver.get("https://www.rdveikals.lv/promoaction/lv/444/page/1/");
+  //      WebElement HuaweiMobile2 = driver.findElement(By.xpath("//div[4]/div/div/ul/li[3]/a"));
+     //   HuaweiMobile2.click();
 
      /*   WebElement careers = driver.findElement(By.id("menu-item-127"));
         careers.click();
